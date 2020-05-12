d328c19f315c8d91dd31053495cf7ee790d18f33
Duy Le <groawp@gmail.com>
2020-05-11 18:27:54 -0700

    Done

---
+++ movie-app/src/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// //STORE
+
+// //ACTION
+// const increment = () => {
+//   return {
+//     type: "INCREMENT"
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT"
+//   }
+// }
+
+// //REDUCER - function return an object that process stuff
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// }
+
+// let store = createStore(counter)
+// store.subscribe(()=> console.log(store.getState()))
+
+// //DISPATCH
+// store.dispatch(increment())
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
